29761d24c6eca81fafe86072e0a51b714397cf7a
howooke <howooke@gmail.com>
2022-07-07 16:25:40 +0900

    수정

--- src/routes/room.js
+++ src/routes/room.js
@@ -132,4 +132,100 @@ router.post("/exit", (req, res, next) => {
 //   }
 // })
 
+
+//카테고리 조회 (미완성)
+router.get('/tag/:tagId', async (req, res)=> {
+  try {
+      const { tagId } = req.params
+      const tags = await Room.find({ tagId }).sort("-createAt")
+      if (!tags) {
+          return res.status(400).json({
+              success: false,
+              msg: "해당 카테고리 방이 존재하지 않습니다."
+          })
+      }
+      res.status(200).json({
+          success: true,
+          tags,
+      })
+  } catch (error) {
+      console.log(error)
+      res.status(400).send({errorMessage: error.message})
+  }
+})
+
+
+//참여중인 스터디 조회
+router.get('/entered-room', authMiddleware, async (req, res)=> {
+  try {
+      const { nickname } = res.locals.user
+      // const checkUser = await Users.findOne({ nickname })
+      const enterRoom = User.find({ roomId }).sort("-createAt")
+
+      if (!nickname === enterRoom.nickname) {
+          return res.status(400).json({ success: false, msg: "참여중인 스터디를 찾을 수 없습니다." })
+      }
+      if (!enterRoom) {
+          return res.status(400).json({ success: false, msg: "해당 카테고리 방이 존재하지 않습니다." })
+      }
+      res.status(200).json({
+          success: true,
+          enterRoom,
+      })
+  } catch (error) {
+      console.log(error)
+      res.status(400).send({ errorMessage: error.message })
+  }
+})
+
+
+//호스트중인 스터디 조회
+router.get('/host-room', authMiddleware, async (req, res)=> {
+  try {
+      const { userId } = res.locals.user
+      const hostRoom = await User.find({ roomId }).sort("-createAt")
+
+      if (!userId === hostRoom.userId) {
+          return res.status(400).json({ success: false, msg: "호스트중인 스터디가 없습니다." })
+      }
+      if (!hostRoom) {
+          return res.status(400).json({ success: false, msg: "스터디를 불러올수없습니다." })
+      }
+      res.status(200).json({
+          success: true,
+          hostRoom
+      })
+  } catch (error) {
+      console.log(error)
+      res.status(400).send({ errorMessage: error.message })
+  }
+  
+})
+
+
+//유저 초대하기
+router.put('/invite', authMiddleware, async (req, res)=> {
+  try {
+      const { nickname } = res.locals.user
+      const { roomId } = req.body
+      const userList = await User.find({ nickname: {$ne: nickname} }).sort("nickname")
+                                      //로그인한 나를 제외한 유저목록조회 (이름 순으로 정렬)
+      if (!userList) { return res.status(400).json({ success: false, msg: "유저 리스트를 불러올 수 없습니다." })}
+      if (userList === nickname) { return res.status(400).json({ success: false, msg: "본인 포함 오류" })}
+
+      const inviteUser = await Room.findOneAndUpdate({ roomId },{ $push: { userNickname: userList.nickname }})
+      res.status(200).json({
+          success: true,
+          inviteUser,
+          msg: "초대 완료"
+      })
+  } catch (error) {
+      console.log(error)
+      res.status(400).send({ errorMessage: error.message })
+  }
+  
+})
+
+
+
 module.exports = router
